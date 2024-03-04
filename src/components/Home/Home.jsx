516f10a62b4564c396e7956c39ce22e2e9c1fb72
Sanja Rudic <rudic.sanja@gmail.com>
2024-03-04 18:22:08 +0100

    Add Home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.Hello}>
+        <TypeAnimation
+          sequence={[
+            "H",
+            500,
+            "HE",
+            500,
+            "HEL",
+            500,
+            "HELL",
+            500,
+            "HELLO",
+            500,
+            "HELLO,",
+            500,
+          ]}
+          cursor={false}
+        />
+      </div>
+
+      <p>I'm Sanja.</p>
+      <p>
+        Welcome to my portfolio website. I'm a passionate web developer with a
+        degree in Computer Science.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
